7b8b2b18b0946827a2507b751e74409e1daa64a7
lishaolin_lsl@126.com <lishaolin_lsl@126.com>
2025-12-16 15:07:46 +0800

    feat: 修复deepinsightChat场景下思考和结果面板和聊天页面宽度不一致的问题

--- web/src/pages/next-chats/chat/chat-box/single-chat-box.tsx
+++ web/src/pages/next-chats/chat/chat-box/single-chat-box.tsx
@@ -402,7 +402,7 @@ export function SingleChatBox({
 
         {/* 右边的思考面板 - 在 deepinsight 模式下显示 */}
         {isDeepinsightMode && thinkingPanelVisible && (
-          <div className="w-96 min-w-96 border-l border-gray-200 overflow-y-auto h-full bg-white flex-shrink-0 flex flex-col">
+          <div className="flex-1 border-l border-gray-200 overflow-y-auto h-full bg-white flex-shrink-0 flex flex-col">
             <DeepInsightThinkingPanel
               data={thinkingData}
               // deepinsightChat 模式下不显示右侧思考/结果面板的加载框
